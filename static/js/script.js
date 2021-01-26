204781067f4935510d2f54a05dec7938a019ff9e
NikolasKotsikos <kotsikn@gmail.com>
2021-01-26 13:48:26 +0000

    Fixed various layout display issues

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('.sidenav').sidenav({ edge: "right" });
-    $('select').formSelect();    
+    $('select').formSelect();   
+    $('.tooltipped').tooltip(); 
 
     validateMaterializeSelect();
     function validateMaterializeSelect() {
